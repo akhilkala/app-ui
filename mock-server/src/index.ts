189199d19e50c1ef98ddcbc22059594b1f1ea878
Akhil Kala <akhilkala@pop-os.localdomain>
2022-01-12 13:47:05 +0530

    init

---
+++ mock-server/src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = 8000;
+const app = express();
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
